abb116e9d662689ba8485df547bbc837c7bae653
AdielTuson <adielt81@gmail.com>
2023-04-20 16:58:28 +0300

    Write a function that plays a round of the game
    I wrote a function that will take the choises of both the player
    and the computer which the return a string  message indicating who
    is the winner of the round, and what choise beats the other. In
    case of a tie, will return a tie message.

--- rps-script.js
+++ rps-script.js
@@ -1,6 +1,7 @@
 //prompt a pop-up box with a message for the user, and ask for his choice
-let userSelection = window.prompt('You are going to play the most ancient and noble game of all time. A game that kings used to settle wars. A game that has been used throughout history to solve the biggest and most complicated conflicts that mankind have ever faced. It is known by many names, but you might know it as ROCK, PAPER, SCISSORS. In this game you will face a brutal opponent, the computer. Choose your weapon.' , 'Rock, Paper, or Scissors');
+let userSelection = window.prompt('You are going to play the most ancient and noble game of all time. A game that kings used to settle wars. A game that has been used throughout history to solve the biggest and most complicated conflicts that mankind have ever faced. It is known by many names, but you might know it as ROCK, PAPER, SCISSORS. In this game you will face a brutal opponent, the computer. Choose your weapon.' , '');
 
+//a function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -18,4 +19,41 @@ function getComputerChoice() {
     }
 }
 
+//assign the getComputerChoice() functions result to a variable
 let computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+/*write a function that will take the user's and the computer's choices,
+evaluate who is the winner and return
+a string that declares the winner of the round.*/
+function playRound(userSelection, computerSelection) {
+    switch(userSelection.toUpperCase()) {
+        case 'ROCK':
+            if (computerSelection === 'Paper')
+                return ("You Lose! Paper beats Rock");
+            else if (computerSelection ==='Scissors')
+                return ("You Win! Rock beats Scissors");
+            else 
+                return "It's a tie!";
+            break;
+        
+        case 'PAPER':
+            if (computerSelection === 'Scissors')
+                return ("You Lose! Scissors beats Paper")
+            else if (computerSelection === 'Rock')
+                return ("You Win! Paper beats Rock")
+            else
+                return "It's a tie!";
+            break;
+
+        case 'SCISSORS':
+            if (computerSelection === 'Rock')
+                return ("You Lose! Rock beats Scissors");
+            else if (computerSelection === 'Paper')
+                return ("You Win! Scissors beats Paper")
+            else 
+                return ("It's a tie!")
+            break;
+    }  
+} 
+console.log(playRound(userSelection, computerSelection));
